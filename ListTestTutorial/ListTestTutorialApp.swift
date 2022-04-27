155ad384ea8f00e1f595c2eaf8831581ba7ecf63
Oncu <oohancan@gmail.com>
2022-04-27 04:36:43 +0300

    Initial Commit

---
+++ ListTestTutorial/ListTestTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListTestTutorialApp.swift
+//  ListTestTutorial
+//
+//  Created by Oncu Ohancan on 27.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ListTestTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
